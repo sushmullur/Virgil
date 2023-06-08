1367c17806c457914c378042288040908ccee22d
Paulo Nascimento <37284051+paulonasc@users.noreply.github.com>
2023-06-07 22:35:30 -0700

    Update update_customer.php - allow updates to show customer

--- update_customer.php
+++ update_customer.php
@@ -7,17 +7,17 @@
  */
 require_once 'config.inc.php';
 
-// Get Customer Number
-$id = $_GET['id'];
-if ($id === "") {
+// Get Job ID
+$jobID = $_GET['jobID'];
+if ($jobID === "") {
     header('location: list_customers.php');
     exit();
 }
-if ($id === false) {
+if ($jobID === false) {
     header('location: list_customers.php');
     exit();
 }
-if ($id === null) {
+if ($jobID === null) {
     header('location: list_customers.php');
     exit();
 }
@@ -45,54 +45,61 @@ require_once 'header.inc.php';
 
     // Check if the form is submitted
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $name = $_POST['jobName'];
-        if ($name === null) {
-            echo "<div><i>Specify a new name</i></div>";
-        } else if ($name === false) {
-            echo "<div><i>Specify a new name</i></div>";
-        } else if (trim($name) === "") {
-            echo "<div><i>Specify a new name</i></div>";
+        $jobName = $_POST['jobName'];
+        $maxLevel = $_POST['maxLevel'];
+        $type = $_POST['type'];
+
+        // Update the job details
+        $sql = "UPDATE Job SET jobName = ?, maxLevel = ?, type = ? WHERE jobID = ?";
+        $stmt = $conn->prepare($sql);
+        if (!$stmt) {
+            echo "Failed to prepare";
         } else {
-            // Update the customer name
-            $sql = "UPDATE Job SET jobName = ? WHERE jobID = ?";
-            $stmt = $conn->prepare($sql);
-            if (!$stmt) {
-                echo "Failed to prepare";
+            // Bind user input to the statement
+            $stmt->bind_param('ssss', $jobName, $maxLevel, $type, $jobID);
+            // Execute the statement
+            $stmt->execute();
+            // Check if the update was successful
+            if ($stmt->affected_rows > 0) {
+                echo "Job details updated successfully.";
             } else {
-                // Bind user input to the statement
-                $stmt->bind_param('ss', $jobName, $jobID);
-                // Execute the statement
-                $stmt->execute();
-                // Check if the update was successful
-                if ($stmt->affected_rows > 0) {
-                    echo "Customer name updated successfully.";
-                } else {
-                    echo "Failed to update customer name.";
-                }
+                echo "Failed to update job details.";
             }
         }
     }
 
-    // Retrieve the customer details
-    $sql = "SELECT CustomerNumber, CustomerName, StreetAddress, CityName, StateCode, PostalCode FROM customer C " .
-        "INNER JOIN address A ON C.defaultAddressID = A.addressID WHERE CustomerNumber = ?";
+    // Retrieve the job details
+    $sql = "SELECT j.jobName, j.jobID, j.maxLevel, j.type, i.itemName
+            FROM Job j
+            JOIN JobItem ji ON j.jobID = ji.jobID
+            JOIN Item i ON ji.itemID = i.itemID
+            WHERE j.jobID = ?";
     $stmt = $conn->prepare($sql);
     if (!$stmt) {
         echo "Failed to prepare";
     } else {
-        $stmt->bind_param('s', $id);
+        $stmt->bind_param('s', $jobID);
         $stmt->execute();
-        $stmt->bind_result($customerNumber, $customerName, $streetName, $cityName, $stateCode, $postalCode);
+        $stmt->bind_result($jobName, $jobID, $maxLevel, $type, $itemName);
         ?>
         <form method="post">
-            <input type="hidden" name="id" value="<?= $id ?>">
-            <?php
-            while ($stmt->fetch()) {
-                echo '<a href="show_customer.php?id=' . $customerNumber . '">' . $customerName . '</a><br>' .
-                    $streetName . ', ' . $cityName . ', ' . $stateCode . ' ' . $postalCode;
-            }
-            ?><br><br>
-            New Name: <input type="text" name="customerName">
+            <input type="hidden" name="jobID" value="<?= $jobID ?>">
+            <div>
+                <label for="jobName">Job Name:</label>
+                <input type="text" name="jobName" value="<?= htmlspecialchars($jobName) ?>">
+            </div>
+            <div>
+                <label for="maxLevel">Max Level:</label>
+                <input type="text" name="maxLevel" value="<?= htmlspecialchars($maxLevel) ?>">
+            </div>
+            <div>
+                <label for="type">Type:</label>
+                <input type="text" name="type" value="<?= htmlspecialchars($type) ?>">
+            </div>
+            <div>
+                <label for="itemName">Item Name:</label>
+                <input type="text" name="itemName" value="<?= htmlspecialchars($itemName) ?>">
+            </div>
             <button type="submit">Update</button>
         </form>
         <?php
