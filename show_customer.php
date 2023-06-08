5f14464be56a5443065a57176e8612c8b34a6099
Paulo Nascimento <37284051+paulonasc@users.noreply.github.com>
2023-06-07 21:54:17 -0700

    Update show_customer.php - 3 way join table

--- show_customer.php
+++ show_customer.php
@@ -6,18 +6,18 @@
  * Time: 3:07 PM
  */
 require_once 'config.inc.php';
-// Get Customer Number
-$id = isset($_GET['id']) ? $_GET['id'] : '';
-if ($id === "") {
-    header('location: list_customers.php');
+// Get Job Name
+$jobName = isset($_GET['jobName']) ? $_GET['jobName'] : '';
+if ($jobName === "") {
+    header('location: list_jobs.php');
     exit();
 }
-if ($id === false) {
-    header('location: list_customers.php');
+if ($jobName === false) {
+    header('location: list_jobs.php');
     exit();
 }
-if ($id === null) {
-    header('location: list_customers.php');
+if ($jobName === null) {
+    header('location: list_jobs.php');
     exit();
 }
 ?>
@@ -43,34 +43,41 @@ require_once 'header.inc.php';
     }
 
     // Prepare SQL using Parameterized Form (Safe from SQL Injections)
-    $sql = "SELECT j.jobName, j.jobID, j.maxLevel, j.type
-        FROM Job j 
+    $sql = "SELECT j.jobName, j.jobID, j.maxLevel, j.type, i.itemName
+        FROM Job j
+        JOIN JobItem ji ON j.jobID = ji.jobID
+        JOIN Item i ON ji.itemID = i.itemID
         WHERE j.jobName = ?";
 
     $stmt = $conn->prepare($sql);
     if (!$stmt) {
-        echo "failed to prepare";
+        echo "Failed to prepare";
     } else {
         // Bind Parameters from User Input
-        $stmt->bind_param('s', $id);
+        $stmt->bind_param('s', $jobName);
 
         // Execute the Statement
         $stmt->execute();
 
         // Process Results Using Cursor
-        $stmt->bind_result($jobName, $jobID, $maxLevel, $type);
+        $stmt->bind_result($jobName, $jobID, $maxLevel, $type, $itemName);
 
         echo "<div>";
         if ($stmt->fetch()) {
-            echo htmlspecialchars($jobName) . "<br>";
+            echo "Job Name: " . htmlspecialchars($jobName) . "<br>";
+            echo "Job ID: " . htmlspecialchars($jobID) . "<br>";
+            echo "Max Level: " . htmlspecialchars($maxLevel) . "<br>";
+            echo "Type: " . htmlspecialchars($type) . "<br>";
+            echo "Item Name: " . htmlspecialchars($itemName) . "<br>";
             while ($stmt->fetch()) {
                 // Additional rows, if any, can be processed here
+                echo "Item Name: " . htmlspecialchars($itemName) . "<br>";
             }
         }
         echo "</div>";
     ?>
         <div>
-            <a href="update_customer.php?id=<?= htmlspecialchars($jobName) ?>">Update Job</a>
+            <a href="update_job.php?jobName=<?= htmlspecialchars($jobName) ?>">Update Job</a>
         </div>
     <?php
     }
